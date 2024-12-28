9fd6c6fbbca8f1cea625ec52ae48da5558a13074
mahldcat <mahldcat@users.noreply.github.com>
2024-12-27 19:16:18 -0700

    Database deploy (#11)
    * apparently deploy database exe does not append .exe in linux
    
    * update so I can view errors on the server (precursor to adding proper logging)

--- ResumeData/Controllers/PaginatedDataController.cs
+++ ResumeData/Controllers/PaginatedDataController.cs
@@ -21,23 +21,53 @@ public class PaginatedDataController(IPaginatedData paginatedData) : Controller
         return "Sample Test";
     }
 
+    private async Task<IActionResult> Handler(Func<Task<object?>> fetchData)
+    {
+        try
+        {
+            var result = await fetchData(); // Await the asynchronous action
+            return Ok(result); // Return the result wrapped in an Ok response
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex.ToString());
+        }
+    }
+
     [Route("data/{id}")]
     [HttpGet]
-    public async Task<PaginatedDataEntry> GetDataEntryById(int id)
+    public async Task<IActionResult> GetDataEntryById(int id)
     {
-        return await PaginatedData.FetchByIdAsync(id);
+        return await Handler(
+        async () =>
+        {
+            return await PaginatedData.FetchByIdAsync(id);
+        });
     }
 
     [Route("data")]
     [HttpGet]
-    public async Task<(IList<PaginatedDataEntry> Entries, int TotalCount)> GetDataEntries(
+    public async Task<IActionResult> GetDataEntries(
         [FromQuery] int pageId, 
         [FromQuery] int maxPageSize)
     {
-        return await PaginatedData.FetchByIdAsync(pageId, maxPageSize);
+        return await Handler(
+        async () =>
+        {
+            var results = await PaginatedData.FetchByIdAsync(pageId, maxPageSize);
+            return new ResultSet
+            {
+                Entries = results.Entries,
+                TotalPages = results.TotalPages
+            };
+        });
+}
+
+
+
+    public struct ResultSet
+    {
+        public IList<PaginatedDataEntry> Entries { get; set; }
+        public int TotalPages { get; set; }
     }
-    
-    
-    
-    
 }
